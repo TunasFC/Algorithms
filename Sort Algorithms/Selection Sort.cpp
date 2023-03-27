31e9f624dd39740ed092b7def29275bbdea8f489
TunasFC <ivanibarra742@gmail.com>
2023-03-27 10:51:58 -0600

    Uploading cpp algorithms

---
+++ Sort Algorithms/Selection Sort.cpp
@@ -0,0 +1,61 @@
+//
+// Created by ivani on 3/26/2023.
+// Code by GeekforGeeks
+
+// C++ program for implementation of
+// selection sort
+#include <bits/stdc++.h>
+using namespace std;
+
+//Swap function
+
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    // One by one move boundary of
+    // unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in
+        // unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        // Swap the found minimum element
+        // with the first element
+        for (int k = 0; k < n; ++k) {
+            cout<<arr[k]<<" ";
+        }
+        cout<<"\n";
+        if (min_idx!=i) swap(arr[min_idx], arr[i]);
+    }
+
+}
+
+//Function to print an array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+
+    }
+    cout << endl;
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
+// This is code is contributed by rathbhupendra
