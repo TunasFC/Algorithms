4f0397a19c0eedf5f4c05ea9997beb8b0057c52e
TunasFC <ivanibarra742@gmail.com>
2023-03-29 20:05:39 -0600

    Create insertion sort

---
+++ Sort Algorithms/insertion sort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int array[6] = {5,2,4,6,1,3};
+    int j, key = 0;
+
+    for (int i = 0; i < 6; ++i) {
+        cout<<array[i]<<" ";
+    }
+
+    cout<<endl;
+
+    for (int i = 1; i < sizeof(array)/sizeof(array[0]); ++i) {
+        j = i-1;
+        key = array[i];
+        while (j >= 0 && array[j] > key){
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j+1] = key;
+    }
+
+    for (int i = 0; i < 6; ++i) {
+        cout<<array[i]<<" ";
+    }
+
+
+}
